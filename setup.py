09c853cef14c9a585b06e4ffda1a72e4e07c1f9a
jonykalavera <mr.jony@gmail.com>
2013-10-17 04:17:39 -0400

    setup file

--- setup.py
+++ setup.py
@@ -1,35 +1,16 @@
-import os
 from setuptools import setup, find_packages
-import pageviews
-
-
-def read(fname):
-    try:
-        return open(os.path.join(os.path.dirname(__file__), fname)).read()
-    except IOError:
-        return ''
-
 
 setup(
-    name="django-pageviews",
-    version=pageviews.__version__,
-    description=read('DESCRIPTION'),
-    long_description=read('README.md'),
-    license='The MIT License',
-    platforms=['OS Independent'],
-    keywords='django, pageviews, generic',
+    version='0.1',
+    description='django-pageviews',
+    long_description=open('README.md').read(),
     author='Jony Kalavera',
     author_email='mr.jony@gmail.com',
-    url="https://github.com/jonykalavera/django-pageviews",
+    name='pageviews',
     packages=find_packages(),
     include_package_data=True,
-    tests_require=[
-        'fabric',
-        'factory_boy',
-        'django-nose',
-        'coverage',
-        'django-coverage',
-        'mock',
+    install_requires=[
+        'django',
     ],
-    test_suite='pageviews.tests.runtests.runtests',
+    license="GPL",
 )
