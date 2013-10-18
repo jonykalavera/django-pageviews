690cceed944ca7b27f3bcb2f56da0d2a941ec726
jonykalavera <mr.jony@gmail.com>
2013-10-18 01:41:17 -0400

    partial commit

--- pageviews/middleware.py
+++ pageviews/middleware.py
@@ -3,9 +3,23 @@ from django.db.models import F
 
 
 class PageViewsMiddleware:
-    def process_request(self, request, *args, **kwargs):
-        hit, hit_created = HitCount.objects.get_or_create(url=request.path)
-        hit.hits = F('hits') + 1
-        hit.save()
+    
+    def process_response(self, request, response, *args, **kwargs):
+        if response.status_code == 200:
+            content_object = getattr(response, 'content_object', None)
+            if content_object:
+                content_type = ContentType.objects.get_for_model(
+                    content_object._model)
+                hit, hit_created = HitCount.objects.get_or_create(
+                    url=request.path, 
+                    object_id=content_object.pk,
+                    content_type=content_type
+                    )
+            else:
+                hit, hit_created = HitCount.objects.get_or_create(url=request.path)
+                
+            
+            hit.hits = F('hits') + 1
+            hit.save()
 
-        return None
+        return response
--- pageviews/models.py
+++ pageviews/models.py
@@ -3,10 +3,16 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class HitCount(models.Model):
+    """
+    Hit count model. An optional related object can be set.
+    """
     created = models.DateTimeField(_('Created'), auto_now_add=True, editable=False)
     modified = models.DateTimeField(_('Modified'), auto_now=True, editable=False)
     url = models.CharField(_('URL'), max_length=2000)
     hits = models.PositiveIntegerField(_('Hits'), default=0)
+    content_type = models.ForeignKey(ContentType, null=True)
+    object_id = models.PositiveIntegerField(null=True)
+    content_object = generic.GenericForeignKey('content_type', 'object_id')
 
     class Meta:
         ordering = ('-created', '-modified')
