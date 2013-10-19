c6e4105a9d24c1d2f2baf08bd699a385ca5050e1
jonykalavera <mr.jony@gmail.com>
2013-10-19 13:08:24 -0500

    fix middleware

--- pageviews/middleware.py
+++ pageviews/middleware.py
@@ -1,24 +1,31 @@
-from .models import HitCount
+# coding: utf-8
+from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
 from django.db.models import F
+
+from .models import HitCount
+
+PAGEVIEWS_OBJECT_ATTR = getattr(
+    settings, 'PAGEVIEWS_OBJECT_ATTR', 'content_object')
 
 
 class PageViewsMiddleware:
-    
+
     def process_response(self, request, response, *args, **kwargs):
         if response.status_code == 200:
-            content_object = getattr(response, 'content_object', None)
+            content_object = getattr(request, PAGEVIEWS_OBJECT_ATTR, None)
             if content_object:
                 content_type = ContentType.objects.get_for_model(
                     content_object._model)
                 hit, hit_created = HitCount.objects.get_or_create(
-                    url=request.path, 
+                    url=request.path,
                     object_id=content_object.pk,
                     content_type=content_type
-                    )
+                )
             else:
-                hit, hit_created = HitCount.objects.get_or_create(url=request.path)
-                
-            
+                hit, hit_created = HitCount.objects.get_or_create(
+                    url=request.path)
+
             hit.hits = F('hits') + 1
             hit.save()
 
--- pageviews/models.py
+++ pageviews/models.py
@@ -1,3 +1,6 @@
+# coding: utf-8
+from django.contrib.contenttypes import generic
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
@@ -6,14 +9,19 @@ class HitCount(models.Model):
     """
     Hit count model. An optional related object can be set.
     """
-    created = models.DateTimeField(_('Created'), auto_now_add=True, editable=False)
-    modified = models.DateTimeField(_('Modified'), auto_now=True, editable=False)
+    created_at = models.DateTimeField(
+        _('Created at'), auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(
+        _('Updated at'), auto_now=True, editable=False)
     url = models.CharField(_('URL'), max_length=2000)
     hits = models.PositiveIntegerField(_('Hits'), default=0)
+    # related content_object
     content_type = models.ForeignKey(ContentType, null=True)
     object_id = models.PositiveIntegerField(null=True)
     content_object = generic.GenericForeignKey('content_type', 'object_id')
 
     class Meta:
-        ordering = ('-created', '-modified')
+        ordering = ('-created_at', '-updated_at')
         get_latest_by = 'created'
+        verbose_name = _(u'Hit')
+        verbose_name_plural = _(u'Hits')
